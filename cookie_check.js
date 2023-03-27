80edbc618efdff4aa3d8d68127d2db7c66027493
dss24 <114159598+dss24@users.noreply.github.com>
2023-03-27 16:16:48 -0400

    Delete cookie_check.js

--- cookie_check.js
+++
@@ -1,7 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('should display cookie popup', async ({ page }) => {
-  await page.goto('https://example.com');
-  const popup = await page.$('.cookie-popup');
-  expect(popup).not.toBeNull();
-});
